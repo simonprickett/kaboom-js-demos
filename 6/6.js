e87086f5929300c0464cd6ea5ead465404284153
Simon Prickett <simon@crudworks.org>
2021-07-19 14:41:05 +0100

    Work in progress step 6.

---
+++ 6/6.js
@@ -0,0 +1,173 @@
+window.onload = function () {
+  // Initialize Kaboom...
+  const k = kaboom({
+    global: true,
+    scale: 3,
+    clearColor: [0, 0, 0, 1],
+    canvas: document.getElementById('game'),
+    width: 180,
+    height: 180
+  });
+
+  // Load the various sprite graphics.
+  loadRoot('/');
+  loadSprite('player', 'sprites/player.png');
+  loadSprite('wall', 'sprites/wall.png');
+  loadSprite('key', 'sprites/key.png');
+  loadSprite('door', 'sprites/door.png');
+  loadSprite('lockeddoor', 'sprites/lockeddoor.png');
+
+  let playerHasKey = false;
+  
+  scene('room', (roomId) => {
+    const roomLayouts = [
+      [ "=====1======",
+        "=          =",
+        "=   k      =",
+        "=          =",
+        "=          =",
+        "=     @    l",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "======2====="
+      ],
+      [ "============",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=     @    =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "======3====="
+      ],
+      [ "======4=====",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=     @    =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "=          =",
+        "============"
+      ]
+    ];
+
+    const layout = roomLayouts[roomId];
+
+    const roomConf = {
+      width: layout[0].length,
+      height: layout.length,
+      pos: vec2(20, 20),
+      '@': [
+        sprite('player'),
+        'player'
+      ],
+      '=': [
+        sprite('wall'),
+        solid()
+      ],
+      'k': [
+        sprite('key'),
+        'key',
+      ],
+      '1': [
+        sprite('door'),
+        solid(),
+        'door',
+        {
+          leadsTo: 1
+        }
+      ],
+      '2': [
+        sprite('door'),
+        solid(),
+        'door',
+        {
+          leadsTo: 2
+        }
+      ],
+      '3': [
+        sprite('door'),
+        solid(),
+        'door',
+        {
+          leadsTo: 0
+        }
+      ],
+      '4': [
+        sprite('door'),
+        solid(),
+        'door',
+        {
+          leadsTo: 0
+        }
+      ],
+      'l': [
+        sprite('lockeddoor'),
+        solid(),
+        'lockeddoor'
+      ]
+    };
+
+    addLevel(layout, roomConf);
+
+    // Delete any key if the player already has it...
+    // TODO
+
+    const player = get('player')[0];
+
+    const directions = {
+      'left': vec2(-1, 0),
+      'right': vec2(1, 0),
+      'up': vec2(0, -1),
+      'down': vec2(0, 1)
+    };
+
+    // Map key presses to player movement actions.
+    for (const direction in directions) {
+		  keyDown(direction, () => {
+        // Move the player.
+			  player.move(directions[direction].scale(60));
+		  });
+	  }
+
+    player.overlaps('door', (d) => {
+      go('room', d.leadsTo);
+    });
+
+    player.overlaps('lockeddoor', (d) => {
+      // TODO open if player has key...
+      camShake(10);
+    });
+
+    player.overlaps('key', (k) => {
+      playerHasKey = true;
+      destroy(k);
+    });
+
+    player.action(() => {
+      player.resolve();
+    });
+  });
+
+  scene('start', () => {
+    add([
+      text('press space to begin!', 6),
+      pos(width() / 2, height() / 2),
+      origin('center'),
+    ]);
+
+    keyPress('space', () => {
+      go('room', 0);
+    });
+  });
+
+  start('start');
+};
